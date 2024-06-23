da7b56c4d3ad29c28d749de6897a1553dca4cf17
manish <manishkr9547@gmail.com>
2024-06-23 18:46:34 +0530

    social media

---
+++ Projects/social-media/src/store/post-list-store.jsx
@@ -0,0 +1,82 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.paylode.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.paylode, ...currPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      paylode: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      paylode: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider
+      value={{
+        postList,
+        addPost,
+        deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Goning to Mumbai",
+    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoy"],
+  },
+  {
+    id: "2",
+    title: "pass ho gai bhai",
+    body: "4 saal ki masti k baad bhi ho gaye hain pass. Hard to beleve",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
+
+export default PostListProvider;
